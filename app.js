e3959eaf8729a88882519f7e4712ee1509f5e42e
Dhaval Varsada <dhaval.v@tasolglobal.com>
2016-12-01 18:11:30 +0530

    remove original source

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var APP_PRIVATE_IP_ADDRESS = 'localhost';
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8081, APP_PRIVATE_IP_ADDRESS);
-console.log('Server running at http://'+APP_PRIVATE_IP_ADDRESS+':8081/');
